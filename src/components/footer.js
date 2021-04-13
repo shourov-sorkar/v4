f975a5922fda1adfc4099acf2b32fc1defbbacbf
shourov-sorkar <18103142@iubat.edu>
2021-04-13 14:52:56 +0600

    :tada:

--- src/components/footer.js
+++ src/components/footer.js
@@ -77,7 +77,7 @@ const Footer = () => {
     if (process.env.NODE_ENV !== 'production') {
       return;
     }
-    fetch('https://api.github.com/repos/bchiang7/v4')
+    fetch('https://api.github.co/repos/bchiang7/v4')
       .then(response => response.json())
       .then(json => {
         const { stargazers_count, forks_count } = json;
@@ -106,7 +106,7 @@ const Footer = () => {
 
       <StyledCredit tabindex="-1">
         <a href="https://github.com/bchiang7/v4">
-          <div>Designed &amp; Built by Brittany Chiang</div>
+          {/* <div>Designed &amp; Built by Brittany Chiang</div> */}
 
           {githubInfo.stars && githubInfo.forks && (
             <div className="github-stats">
